43a1e7f76424bfb4a8af181b007ca2070b09f1e1
Ellie <ellie.galdr@gmail.com>
2023-08-01 12:00:22 -0700

    fix signin & out

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,9 @@
 class RoomsController < ApplicationController
   def index
+    @current_user = current_user
+    redirect_to '/signin' unless @current_user
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
   end
   def show
     @current_user = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
+  get '/signout', to: 'sessions#destroy'
   resources :rooms
   resources :users
   root 'rooms#index'
